5d39a81fbee4c2bdb5f26da7d74dec19b7c43201
Pedro Thiago <pedrothiagom456@gmail.com>
2026-05-09 14:17:55 -0300

    feat: integrate order service into application flow

--- src/main/java/com/devsuperior/valortotal/exercicio/Application.java
+++ src/main/java/com/devsuperior/valortotal/exercicio/Application.java
@@ -1,13 +1,40 @@
 package com.devsuperior.valortotal.exercicio;
 
+import com.devsuperior.valortotal.exercicio.entities.Order;
+import com.devsuperior.valortotal.exercicio.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Locale.setDefault(Locale.US);
+		Scanner reader = new Scanner(System.in);
+
+		int code = reader.nextInt();
+		double basic = reader.nextDouble();
+		double discount = reader.nextDouble();
+
+		Order order = new Order(code, basic, discount);
+
+		System.out.printf("Pedido código %d\n", order.getCode());
+		System.out.printf("Valor total: R$ %.2f", orderService.total(order));
+
+	}
 }
