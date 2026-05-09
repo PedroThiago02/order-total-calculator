762bdae7a90e9034a8ffba443d7988922f7cfc8e
Pedro Thiago <pedrothiagom456@gmail.com>
2026-05-09 14:14:09 -0300

    feat: implement shipping service

---
+++ src/main/java/com/devsuperior/valortotal/exercicio/services/ShippingService.java
@@ -0,0 +1,19 @@
+package com.devsuperior.valortotal.exercicio.services;
+
+import com.devsuperior.valortotal.exercicio.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+    public double shipment(Order order){
+        if(order.getBasic() < 100){
+            return 20.0;
+        } else if(order.getBasic() >= 100 && order.getBasic() < 200){
+            return 12.0;
+        } else {
+            return 0.0;
+        }
+    }
+
+}
